e7845eb0abf3dac3c5d222164de94ce6cee7bfc8
Bill Fitzgerald <Bill@Macs-MacBook-Pro.local>
2018-01-14 15:08:39 -0500

    Fix error message  on page load

--- internetBS-api/forms/domain-check-form.php
+++ internetBS-api/forms/domain-check-form.php
@@ -3,7 +3,7 @@
     <div class="form-table">
         <span>Enter A Domain Name</span>
         <input type="text" name="internetbs_domain"
-               value="<?php echo isset($_POST['internetbs_domain']) ? $_POST['internetbs_domain'] : '' ?>"/>
+               value="<?php echo isset($_POST['internetbs_domain']) ? $_POST['internetbs_domain'] : '' ?>"required/>
 
     </div>
     <?php submit_button('Check The Domain'); ?>
@@ -28,17 +28,17 @@ curl_setopt_array($curl, array(
         'ResponseFormat' => 'JSON',
     ),
 ));
-
 curl_exec($curl);
 $response = curl_exec($curl);
 $err = curl_error($curl);
 curl_close($curl);
 $obj= json_decode($response , true );
-
-        if($obj["status"]== "UNAVAILABLE"){
-           echo "Sorry, that domain is not available. Please try another";
-        }elseif($obj["status"]== "FAILURE"){
-            echo "Sorry, something went horribly wrong! Please try again";
-        }else{
-            echo "Great, that domain is available. Register it now? ";
-        }
+if (isset($_POST['submit'])) {
+    if ($obj["status"] == "UNAVAILABLE") {
+        echo "Sorry, that domain is not available. Please try another";
+    } elseif ($obj["status"] === "FAILURE") {
+        echo "Sorry, something went horribly wrong! Please try again <br>";
+    } else {
+        echo "Great, that domain is available. Register it now? ";
+    }
+}
