be92c97cd7701101d6ac569de60ab8381bb22e4b
Bill Fitzgerald <Bill@Macs-MacBook-Pro.local>
2018-01-14 14:27:57 -0500

    Cleanup internetBS-api/forms/domain-registration-form.php:

--- internetBS-api/forms/domain-registration-form.php
+++ internetBS-api/forms/domain-registration-form.php
@@ -1,50 +1,57 @@
-
 <form method="post" action="#tabs-2">
     <div class="form-table">
         Domain name:<br>
         <input type="text" name="internetbs_domain"
-               value="<?php echo isset($_POST['internetbs_domain']) ? $_POST['internetbs_domain'] : '' ?>"required/><br>
+               value="<?php echo isset($_POST['internetbs_domain']) ? $_POST['internetbs_domain'] : '' ?>"
+               required/><br>
         First name:<br>
         <input type="text" name="Registrant_FirstName"
-               value="<?php echo isset($_POST['Registrant_FirstName']) ? $_POST['Registrant_FirstName'] : '' ?>"required><br>
+               value="<?php echo isset($_POST['Registrant_FirstName']) ? $_POST['Registrant_FirstName'] : '' ?>"
+               required><br>
         Last name:<br>
         <input type="text" name="Registrant_Lastname"
-               value="<?php echo isset($_POST['Registrant_Lastname']) ? $_POST['Registrant_Lastname'] : '' ?>"required>
+               value="<?php echo isset($_POST['Registrant_Lastname']) ? $_POST['Registrant_Lastname'] : '' ?>" required>
         <br>
         Email:<br>
         <input type="email" name="registrant_email"
-               value="<?php echo isset($_POST['registrant_email']) ? $_POST['registrant_email'] : '' ?>"required><br>
+               value="<?php echo isset($_POST['registrant_email']) ? $_POST['registrant_email'] : '' ?>" required><br>
         Phone Number:<br>
         <input type="tel" name="registrant_phonenumber" placeholder="Format: 1.9999999999"
-               value="<?php echo isset($_POST['registrant_phonenumber']) ? $_POST['registrant_phonenumber'] : '' ?>"required><br>
+               value="<?php echo isset($_POST['registrant_phonenumber']) ? $_POST['registrant_phonenumber'] : '' ?>"
+               required><br>
         Street:<br>
         <input type="text" name="registrant_street"
-               value="<?php echo isset($_POST['registrant_street']) ? $_POST['registrant_street'] : '' ?>"required><br>
+               value="<?php echo isset($_POST['registrant_street']) ? $_POST['registrant_street'] : '' ?>" required><br>
         City:<br>
         <input type="text"
-               name="registrant_city" value="<?php echo isset($_POST['registrant_city']) ? $_POST['registrant_city'] : '' ?>"required
+               name="registrant_city"
+               value="<?php echo isset($_POST['registrant_city']) ? $_POST['registrant_city'] : '' ?>" required
         "><br>
 
 
         State/Provence:<br>
         <input type="text" maxlength="2" size="2" placeholder="VA"
-               name="registrant_state" value="<?php echo isset($_POST['registrant_state']) ? $_POST['registrant_state'] : '' ?>"required
+               name="registrant_state"
+               value="<?php echo isset($_POST['registrant_state']) ? $_POST['registrant_state'] : '' ?>" required
         "><br>
         Postal Code:<br>
         <input type="number"
-               name="registrant_postalcode" value="<?php echo isset($_POST['registrant_postalcode']) ? $_POST['registrant_postalcode'] : '' ?>"required
+               name="registrant_postalcode"
+               value="<?php echo isset($_POST['registrant_postalcode']) ? $_POST['registrant_postalcode'] : '' ?>"
+               required
         "><br>
         Country Code:<br>
         <input type="text" maxlength="2" size="2" placeholder="US"
-               name="registrant_countrycode" value="<?php echo isset($_POST['registrant_countrycode']) ? $_POST['registrant_countrycode'] : '' ?>"required
+               name="registrant_countrycode"
+               value="<?php echo isset($_POST['registrant_countrycode']) ? $_POST['registrant_countrycode'] : '' ?>"
+               required
         "><br>
 
     </div>
-    <?php submit_button('Check The Domain'); ?>
+    <?php submit_button('Register The Domain'); ?>
 
 </form>
 <?php
-
 
 
 $curl = curl_init();
@@ -57,13 +64,13 @@ curl_setopt_array($curl, array(
     CURLOPT_TIMEOUT => 30,
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
     CURLOPT_CUSTOMREQUEST => "POST",
-    CURLOPT_POSTFIELDS => array (
-        'ApiKey' =>  get_option('internet_api_key'),
+    CURLOPT_POSTFIELDS => array(
+        'ApiKey' => get_option('internet_api_key'),
         'Password' => get_option('internet_pass'),
         'domain' => $_POST['internetbs_domain'],
         'ResponseFormat' => 'JSON',
         'Registrant_FirstName' => $_POST['Registrant_FirstName'],
-        'Registrant_Lastname' =>  $_POST['Registrant_Lastname'],
+        'Registrant_Lastname' => $_POST['Registrant_Lastname'],
         'registrant_email' => $_POST['registrant_email'],
         'registrant_phonenumber' => $_POST['registrant_phonenumber'],
         'registrant_street' => $_POST['registrant_street'],
@@ -72,7 +79,7 @@ curl_setopt_array($curl, array(
         'registrant_postalcode' => $_POST['registrant_postalcode'],
         'registrant_state' => $_POST['registrant_state'],
         'technical_FirstName' => $_POST['Registrant_FirstName'],
-        'technical_Lastname' =>  $_POST['Registrant_Lastname'],
+        'technical_Lastname' => $_POST['Registrant_Lastname'],
         'technical_email' => $_POST['registrant_email'],
         'technical_phonenumber' => $_POST['registrant_phonenumber'],
         'technical_street' => $_POST['registrant_street'],
@@ -81,7 +88,7 @@ curl_setopt_array($curl, array(
         'technical_postalcode' => $_POST['registrant_postalcode'],
         'technical_state' => $_POST['registrant_state'],
         'billing_FirstName' => $_POST['Registrant_FirstName'],
-        'billing_Lastname' =>  $_POST['Registrant_Lastname'],
+        'billing_Lastname' => $_POST['Registrant_Lastname'],
         'billing_email' => $_POST['registrant_email'],
         'billing_phonenumber' => $_POST['registrant_phonenumber'],
         'billing_street' => $_POST['registrant_street'],
@@ -90,7 +97,7 @@ curl_setopt_array($curl, array(
         'billing_postalcode' => $_POST['registrant_postalcode'],
         'billing_state' => $_POST['registrant_state'],
         'admin_FirstName' => $_POST['Registrant_FirstName'],
-        'admin_Lastname' =>  $_POST['Registrant_Lastname'],
+        'admin_Lastname' => $_POST['Registrant_Lastname'],
         'admin_email' => $_POST['registrant_email'],
         'admin_phonenumber' => $_POST['registrant_phonenumber'],
         'admin_street' => $_POST['registrant_street'],
@@ -110,14 +117,7 @@ if ($err) {
     echo "cURL Error #:" . $err;
 } else {
     echo "<br>";
-    $obj= json_decode($response , true );
-    //echo "New Test <br>";
-    echo  $obj["product"][0]["message"];
-   // echo "<br>";
-   // echo  $obj["product"][0]["message"] . "<br>";
-    //echo "VAR Dump <br>";
-    //print_r($obj);
-
-    //echo $response;
+    $obj = json_decode($response, true);
+    echo $obj["product"][0]["message"];
 }
 
